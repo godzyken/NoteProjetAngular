1ca0157e85a33af97190d371a1b182f91dfee816
godzyken <isgodzy@msn.com>
2018-11-06 18:18:20 +0100

    verifier les todos
    Signed-off-by: godzy <isgodzy@msn.com>

--- src/app/matieres/matiere.service.ts
+++ src/app/matieres/matiere.service.ts
@@ -1,15 +1,24 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {ToastrService} from 'ngx-toastr';
-import {Router} from '@angular/router';
+/*import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';*/
+
+import {environment} from '../../environments/environment';
+import {Observable} from 'rxjs';
+import {Matiere} from './matieres.interface';
+import {map} from 'rxjs/internal/operators';
 
 
-@Injectable
+@Injectable()
 export class MatiereService {
-  constructor(private http: HttpClient, private toastr: ToastrService, private router: Router) { }
-  url = 'http://localhost:3000';
+  constructor(private http: HttpClient) { }
 
-  getMatiere() {
+/*
+  private toastr: ToastrService, private router: Router
+  url = 'http://localhost:3000';
+*/
+
+  /*getMatiere() {
     return this
       .http
       .get(`${this.url}/matieres`);
@@ -28,5 +37,17 @@ export class MatiereService {
           this.toastr.error(err.message, 'Error occured');
         }
       );
+  }*/
+  getMatiere(): Observable<Matiere[]> {
+    return  this.http.get<Matiere[]>(`${environment.apiUrl}/matieres`);
+  }
+
+  createMatiere(body): Observable<Matiere> {
+    return this.http.post<Matiere>(`${environment.apiUrl}/matieres`, body);
+  }
+
+  deleteMatiere(id): Observable<number> {
+    return this.http.post<Matiere>(`${environment.apiUrl}/matieres/delete`, {'id': id})
+      .pipe(map(response => id));
   }
 }
--- src/app/matieres/matieres.component.ts
+++ src/app/matieres/matieres.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Matiere } from './matieres.interface';
+/*
 import {MatiereService} from './matiere.service';
+*/
 import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AppState} from '../store';
+import {select, Store} from '@ngrx/store';
+import {MatiereListModule} from '../store/actions/matiere.action';
+import {selectMatiereListEntitiesConverted$, selectMatieresLoading$} from '../store/selectors/matiere.selector';
 
 @Component({
   selector: 'app-matieres',
@@ -10,7 +17,7 @@ import {Router} from '@angular/router';
 })
 export class MatieresComponent implements OnInit {
 
-  matieres: Matiere[];
+ /* matieres: Matiere[];
 
   constructor(private matiereSevice: MatiereService, private router: Router) { }
 
@@ -20,10 +27,29 @@ export class MatieresComponent implements OnInit {
       .subscribe((data: Matiere[]) => {
         this.matieres = data;
       });
+  }*/
+
+  public matieres$: Observable<Matiere[]>;
+  public  matieresLoading: Observable<boolean>;
+
+  constructor(private router: Router, private store: Store<AppState>) {
+    this.matieres$ = store
+      .pipe(select(selectMatiereListEntitiesConverted$));
+
+    this.matieresLoading = store.pipe(select(selectMatieresLoading$));
   }
 
-  goToAddMatiere() {
+  ngOnInit() {
+    this.store.dispatch(new  MatiereListModule.LoadInitMatieres());
+  }
+
+  goToAddMatiere () {
     this.router.navigateByUrl('/ajout-matiere');
   }
+
+  deleteMatiere(id: number) {
+    this.store.dispatch(new MatiereListModule.LoadDeleteMatiere(id));
+  }
+
 
 }
