2c0088c317defb816015c4a08aa819c73201da3c
godzyken <isgodzy@msn.com>
2018-11-06 22:46:46 +0100

    poursuite du test
    Signed-off-by: godzyken <isgodzy@msn.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {
   MatToolbarModule,
@@ -52,7 +51,6 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
     MatToolbarModule,
