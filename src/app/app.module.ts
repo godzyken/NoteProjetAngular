ef3710d76359703f1f0c80e20796a34c1c18f851
godzyken <isgodzy@msn.com>
2018-11-06 16:18:14 +0100

    first comit
    Signed-off-by: godzy <isgodzy@msn.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,73 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LayoutModule} from '@angular/cdk/layout';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {MatieresComponent} from './matieres/matieres.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {ToastrModule} from 'ngx-toastr';
+import {MatiereService} from './matieres/matiere.service';
+import {AjoutMatieresComponent} from './ajout-matieres/ajout-matieres.component';
+import {FormsModule} from '@angular/forms';
+import {ConfirmationPopoverModule} from 'angular-confirmation-popover';
+
+const appRoutes: Routes = [
+  {path: 'matiere', component: MatieresComponent},
+  {path: 'ajout-matiere', component: AjoutMatieresComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MatieresComponent,
+    AjoutMatieresComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    ToastrModule.forRoot(),
+    FormsModule,
+    ConfirmationPopoverModule.forRoot({
+      confirmButtonType: 'danger'
+    })
   ],
-  providers: [],
+  providers: [
+    MatiereService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/matieres/matieres.interface.ts
@@ -0,0 +1,5 @@
+export interface Matiere {
+  id_matiere: Number;
+  libelle: String;
+  coefficient: Number;
+}
