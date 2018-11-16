d4c4ef9f7dc181f2b2060debdd4dae4a3b2f69eb
godzyken <isgodzy@msn.com>
2018-11-16 11:52:57 +0100

    karma test en cours Gestions des imports: - dashboard.component.spec.ts:{    StoreModule.forRoot({toString}), }
    - matiere.service.ts:{}
    
    - matiere.component.spec.ts:{
       providers: [Store, StateObservable, MatiereService]
    }
    - matiere.component.ts:{ }
    
    - app.component.spec.ts:{
      provide: MatiereService
     }
    
    Signed-off-by: godzy <isgodzy@msn.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import {Component} from '@angular/core';
 import {MatiereService} from './matieres/matiere.service';
+import anything = jasmine.anything;
+import any = jasmine.any;
 
 
 @Component({selector: 'app-dashboard', template: ''})
@@ -33,14 +35,17 @@ describe('AppComponent', () => {
   it(`should have as title 'NoteProjetAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('NoteProjetAngular');
+
+    expect(app.title).toEqual('NoteProjetAngular').then(fixture => fixture | fixture);
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-   //expect(compiled.querySelector('h1').textContent).toContain('Welcome to NoteProjetAngular!');
-    expect.extend({toBeInTheDOM});
-  });
+    expect(compiled
+      .querySelector('h1')
+      .textContent)
+      .toContain(`Welcome to NoteProjetAngular! ${AppComponent}!`);
+  }));
 });
